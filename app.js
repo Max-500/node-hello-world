2754b5a9c75482f2d35129cbb6998f0dbac92a0c
Max-500 <213497@ids.upchiapas.edu.mx>
2024-06-03 23:00:38 -0600

    First Commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const port = process.env.PORT || 3000;
+
+app.get('/hello', (req, res) => {
+    res.send("Hello World")
+});
+
+
+app.listen(port, () => {
+    console.log(`SERVER RUNNING IN http://localhost:${port}.`);
+});
